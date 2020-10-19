584987044066bf4d351c144ff1d8efe707128f7f
Xcose <mfecanexhanti@gmail.com>
2020-10-19 23:52:07 +0200

    Scroll up button visibility depending on scroll

---
+++ src/Components/Shared/ScrollUp.jsx
@@ -0,0 +1,31 @@
+import { faHandMiddleFinger } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
+import { Button } from "reactstrap";
+
+const ScrollUp = () => {
+	const [isVisible, setIsVisible] = useState(false);
+
+	const changeIsVisible = () => {
+		console.log(window.outerHeight);
+		if (window.scrollY >= window.outerHeight) {
+			setIsVisible(true);
+		} else {
+			setIsVisible(false);
+		}
+	};
+
+	window.addEventListener("scroll", changeIsVisible);
+	return (
+		<Button
+			style={{ position: "fixed", bottom: 100, right: 20, zindex: 999999 }}
+			className={`rounded-circle scroll-up-btn ${isVisible ? "active" : ""}`}
+			onClick={() => {
+				window.scroll({ top: 0, behavior: "smooth" });
+			}}
+		>
+			^
+		</Button>
+	);
+};
+
+export default ScrollUp;
